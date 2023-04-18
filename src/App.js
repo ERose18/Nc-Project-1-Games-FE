239d50c0bb0e99d2af3f6633765176dc435892a1
ERose18 <evie.nulty@gmail.com>
2023-04-18 09:35:08 +0100

    resolving conflicts

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import './App.css';
+import Header from './components/Header';
 import {Routes, Route} from 'react-router-dom';
 import Home from './components/Home';
-import Header from './components/Header';
+import Reviews from './components/Reviews';
 
 function App() {
   return (
-    <div className="App"> 
-    <Header/>
-    <Routes>
-      <Route path="/" element={<Home />}/>
-    </Routes>
+    <div>
+      <Header/>
+       <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/reviews" element={<Reviews />}/>
+      </Routes>
     </div>
   );
 }
