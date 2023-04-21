d3520c48b4f037884ed6d833a075df835a8be446
ERose18 <evie.nulty@gmail.com>
2023-04-21 09:48:24 +0100

    added Welcome {username} to header when logged in, completed ticket 9

--- src/api.js
+++ src/api.js
@@ -4,8 +4,8 @@ const ncGamesAPI = axios.create({
     baseURL: 'https://nc-project-games-r6n8.onrender.com/api'
   });
 
-  export const fetchReviews = () => {
-    return ncGamesAPI.get(`/reviews`)
+  export const fetchReviews = (category) => {
+    return ncGamesAPI.get(`/reviews`, {params: {category}})
     .then((response) => {
         return response.data.reviews;
     })
